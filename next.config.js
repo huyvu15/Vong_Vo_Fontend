917fc70af49290ad08f68fbe947b10116b501cc2
Vũ Văn Huy <vanhuy.vu@tp.com.vn>
2024-11-23 11:03:47 +0700

    add

--- next.config.js
+++ next.config.js
@@ -2,8 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['i.ibb.co','res.cloudinary.com'],
+    domains: ['i.ibb.co', 'res.cloudinary.com'],
   },
-}
+  i18n: {
+    locales: ['vi', 'en'],
+    defaultLocale: 'vi',
+  },
+  basePath: '', // Nếu có basePath, hãy thêm ở đây
+  trailingSlash: false, // Thêm dấu `/` ở cuối URL nếu cần
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
